a5c6830045de9c935db3164d20bb872acfc2b95a
amberabege <amberjacob2002@gmail.com>
2026-07-14 13:09:40 +0100

    calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /,): ")
+if operation == "+":
+    print(first_number + second_number)
+elif operation == "-":
+    print(first_number - second_number)
+elif operation == "*":
+    print(first_number * second_number)
+elif operation =="/":
+    print(first_number/second_number)
+else:
+    print("Invalid operation")
